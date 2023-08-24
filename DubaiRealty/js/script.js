389521f892a7faea2b408ff91f2960e8535378c3
Marina_Gor <ilwing47@gmail.com>
2023-08-24 18:40:18 +0300

    main_maket

--- DubaiRealty/js/script.js
+++ DubaiRealty/js/script.js
@@ -5,8 +5,8 @@ $(document).ready(function () {
       arrows: false,
       dots: true,
       adaptiveHeight: true,
-      slidesToShow: 2,
-      slidesToScroll: 2,
+      slidesToShow: 3,
+      slidesToScroll: 3,
       speed: 1000,
       easinf: 'ease',
       infinite: true,                     //стоп в слайде
@@ -21,12 +21,37 @@ $(document).ready(function () {
       touchMove: true,
       waitForAnimamate:true,
       centerMode:true,
-      centerPadding: "370px"
+      centerPadding: "10px"
 });
 })
 
 $(document).ready(function () {
    $('.section2-slider').slick({
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      speed: 1000,
+      easinf: 'ease',
+      infinite: false,                     //стоп в слайде
+      // autoplay: false,
+      // autoplaySpeed: 1000,
+      // pauseOnFocus: true,
+      // pauseOnHover: true,
+      // pauseOnDotsFocus: true,     //стоп автопроигрования
+      draggable: false,
+      swipe: true,
+      // touchThreshold: 10,
+      touchMove: true,
+      waitForAnimamate:true,
+      // centerMode:true,
+      variableWidth:false,
+});
+})
+
+$(document).ready(function () {
+   $('.section2-container-slider').slick({
       arrows: false,
       dots: true,
       adaptiveHeight: true,
