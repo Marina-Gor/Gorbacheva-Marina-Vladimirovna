57fc10c5a8ade17e622b57a3012ab328eaeaaeb0
Marina_Gor <ilwing47@gmail.com>
2023-08-24 07:36:31 +0300

    mainproject

---
+++ DubaiRealty/js/script.js
@@ -0,0 +1,52 @@
+// Слайдер s8 main
+
+$(document).ready(function () {
+   $('.section8-container-slider').slick({
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      slidesToShow: 2,
+      slidesToScroll: 2,
+      speed: 1000,
+      easinf: 'ease',
+      infinite: true,                     //стоп в слайде
+      autoplay: false,
+      autoplaySpeed: 1000,
+      pauseOnFocus: true,
+      pauseOnHover: true,
+      pauseOnDotsFocus: true,     //стоп автопроигрования
+      draggable: false,
+      swipe: true,
+      touchThreshold: 10,
+      touchMove: true,
+      waitForAnimamate:true,
+      centerMode:true,
+      centerPadding: "370px"
+});
+})
+
+$(document).ready(function () {
+   $('.section2-slider').slick({
+      arrows: false,
+      dots: true,
+      adaptiveHeight: true,
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      speed: 1000,
+      easinf: 'ease',
+      infinite: false,                     //стоп в слайде
+      // autoplay: false,
+      // autoplaySpeed: 1000,
+      // pauseOnFocus: true,
+      // pauseOnHover: true,
+      // pauseOnDotsFocus: true,     //стоп автопроигрования
+      draggable: false,
+      swipe: true,
+      // touchThreshold: 10,
+      touchMove: true,
+      waitForAnimamate:true,
+      // centerMode:true,
+      // centerPadding: "370px"
+      variableWidth:false,
+});
+})
