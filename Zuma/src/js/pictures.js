ba3601bb598c781e7f7e7798eaefbc7d66cff717
Marina_Gor <ilwing47@gmail.com>
2024-02-10 23:51:28 +0300

    FD1_FD2_ZUMA

---
+++ Zuma/src/js/pictures.js
@@ -0,0 +1,21 @@
+import logo from '../assets/images/zuma_logo.png';
+import error from '../assets/images/error.png';
+import blueBall from '../assets/images/ball/blueBall.jpg';
+import yellowBall from '../assets/images/ball/yellowBall.jpg';
+import redBall from '../assets/images/ball/redBall.jpg';
+import greenBall from '../assets/images/ball/greenBall.jpg';
+import track from '../assets/images/track.jpg';
+import frog from '../assets/images/frog.png';
+
+
+
+export {
+	logo,
+	error,
+	blueBall,
+	yellowBall,
+	redBall,
+	greenBall,
+	track,
+	frog,
+}
